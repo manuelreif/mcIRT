2d34eb41d446053e6048c644002be194461e5a04
Manuel Reif <manuel.reif@gmail.com>
2014-01-19 21:03:11 +0100

    added logLik method for nrm and nelm

--- R/deviance.nelm.R
+++ R/deviance.nelm.R
@@ -1,9 +1,30 @@
 deviance.nelm <-
 function(object, ...)
 {
-  nme  <- length(RESnlm$erg_distr$mean_est) - 1
-  nva  <- RESnlm$ctrl$sigmaest *(length(RESnlm$erg_distr$sig_est) -1)
-  npar <- ncol(RESnlm$reshOBJ$Qmat) + nme + nva - length(RESnlm$ctrl$Clist)
+  nme  <- length(object$erg_distr$mean_est) - 1
+  nva  <- object$ctrl$sigmaest *(length(object$erg_distr$sig_est) -1)
+  npar <- ncol(object$reshOBJ$Qmat) + nme + nva - length(object$ctrl$Clist)
   
 structure(2*object$last_mstep$value, df=npar)
 }
+
+
+
+## logLik ##
+
+logLik.nelm <- 
+  function(object,...)
+  {
+    
+    ### number of parameters
+    nme  <- length(object$erg_distr$mean_est) - 1
+    nva  <- object$ctrl$sigmaest * (length(object$erg_distr$sig_est) -1)
+    npar <- ncol(object$reshOBJ$Qmat) + nme + nva  - length(object$ctrl$Clist)
+    # number of observations
+    nobs <- sum(sapply(object$reshOBJ$d,nrow))
+    
+    return(structure((-1)*object$last_mstep$value, df=npar, nobs=nobs))
+    
+  }
+
+
--- R/deviance.nrm.R
+++ R/deviance.nrm.R
@@ -1,11 +1,38 @@
+## deviance ##
+
 deviance.nrm <-
 function(object, ...)
 {
   ### number of parameters
-  nme  <- length(RESnrm$erg_distr$mean_est) - 1
-  #RESnrm$ctrl$sigmaest
-  nva  <- RESnrm$ctrl$sigmaest * (length(RESnrm$erg_distr$sig_est) -1)
-  npar <- ncol(RESnrm$reshOBJ$Qmat) + nme + nva  - length(RESnrm$ctrl$Clist)
+  nme  <- length(object$erg_distr$mean_est) - 1
+  #object$ctrl$sigmaest
+  nva  <- object$ctrl$sigmaest * (length(object$erg_distr$sig_est) -1)
+  npar <- ncol(object$reshOBJ$Qmat) + nme + nva  - length(object$ctrl$Clist)
   
 structure(2*object$last_mstep$value, df=npar)
 }
+
+
+## logLik ##
+
+logLik.nrm <- 
+  function(object,...)
+{
+
+### number of parameters
+nme  <- length(object$erg_distr$mean_est) - 1
+nva  <- object$ctrl$sigmaest * (length(object$erg_distr$sig_est) -1)
+npar <- ncol(object$reshOBJ$Qmat) + nme + nva  - length(object$ctrl$Clist)
+# number of observations
+nobs <- sum(sapply(object$reshOBJ$d,nrow))
+
+return(structure((-1)*object$last_mstep$value, df=npar, nobs=nobs))
+
+}
+
+
+
+
+
+
+
