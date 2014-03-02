65676f55cbe7da9b3af712232be0034c126e3bb2
Manuel Reif <manuel.reif@gmail.com>
2014-03-02 11:02:42 +0100

    DDF export which yields nonparametric DDF measures of the standardization approach

--- R/npar_test.R
+++ R/npar_test.R
@@ -81,7 +81,8 @@ wm_all <- mapply(function(daf,d01)
       if(is.na(inn)) 0
       else inn
     })
-  },dd=1:ncol(daf),fl=faclev,SIMPLIFY=FALSE)
+    unlist(prp2)
+  },dd=1:ncol(daf),fl=faclev,SIMPLIFY=TRUE)
   
 }, daf=reshOBJ$d, d01 = reshOBJ$recm,SIMPLIFY=FALSE)
 
@@ -90,9 +91,14 @@ wm_all <- mapply(function(daf,d01)
 
 if(wm == "focal")
 {
-wmc <- wm_all[[2]]  
-  
-}
+wmc <- wm_all[[2]]
+} else if(wm == "reference")
+  {
+    wmc <- wm_all[[1]]  
+  } else if(wm == "total")
+      {
+      wmc <- wm_all[[1]] + wm_all[[2]]
+      }
 
 ##### standardized p-difference #################
 
@@ -102,15 +108,39 @@ items <- ncol(reshOBJ$d[[1]])
 
 stdpdif <- lapply(1:items,function(alli)
       {
-      totsum <- sum(unlist(wmc[[alli]]))
+      totsum <- sum(wmc[,alli])
       # w*(reference - focal)/sum(w)
-      colSums((t(Ps[[1]][[alli]]-Ps[[2]][[alli]]) * unlist(wmc[[alli]]))/totsum)
+      colSums((t(Ps[[1]][[alli]]-Ps[[2]][[alli]]) * unlist(wmc[,alli]))/totsum)
     })
 
 
-##### RMWSD #################
+#####   RMWSD   #################
 
-stdpdif
+
+rmwsd <- lapply(1:items,function(alli)
+{
+  totsum <- sum(wmc[,alli])
+  # w*(reference - focal)/sum(w)
+  sqrt(colSums((t(Ps[[1]][[alli]]-Ps[[2]][[alli]])^2 * wmc[,alli])/totsum))
+})
+
+
+##### VAR(P_fs - P_bs) #################
+
+
+varpp <- mapply(function(ro,di)
+          {
+          ro^2 - di^2
+          },ro=rmwsd,di=stdpdif,SIMPLIFY=FALSE)
+
+
+# Beschriftung
+names(stdpdif) <- colnames(reshOBJ$d[[1]])
+names(rmwsd) <- colnames(reshOBJ$d[[1]])
+names(varpp) <- colnames(reshOBJ$d[[1]])
+
+return(list(stdpdif=stdpdif,rmwsd=rmwsd,varpp=varpp,Ps=Ps))
+
 }
 
 
